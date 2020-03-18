47c2b5fd779cb701ea7b531fc03d4876aa088aff
deshario <deshario9@gmail.com>
2020-03-18 17:38:25 +0700

    Ganache Network Added

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
+    develop: { // truffle
       port: 8545
     },
     ganache: { // Ganache
